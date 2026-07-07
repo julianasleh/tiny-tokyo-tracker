430b4fba1da8c2e8393f3786401bfeaec62336cd
julianasleh <julianasleh@googlemail.com>
2026-07-07 14:38:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // selbst beantwortet (siehe window.fetch-Override in index.html) -- der Service
 // Worker muss dafuer nichts mehr tun.
 
-const CACHE_VERSION = 'ttt-shell-v47';
+const CACHE_VERSION = 'ttt-shell-v48';
 const APP_SHELL = [
   './',
   './index.html',
